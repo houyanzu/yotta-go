923f65102f1bf74c0c12d0009031fde650b6adbb
Your Name <you@example.com>
2020-06-30 11:46:10 +0800

    .

--- responses.go
+++ responses.go
@@ -200,19 +200,18 @@ type GetTableByScopeResp struct {
 }
 
 type GetTableRowsRequest struct {
-	Code          string `json:"code"` // Contract "code" account where table lives
-	Scope         string `json:"scope"`
-	Table         string `json:"table"`
-	LowerBound    string `json:"lower_bound,omitempty"`
-	UpperBound    string `json:"upper_bound,omitempty"`
-	Limit         uint32 `json:"limit,omitempty"`          // defaults to 10 => chain_plugin.hpp:struct get_table_rows_params
-	KeyType       string `json:"key_type,omitempty"`       // The key type of --index, primary only supports (i64), all others support (i64, i128, i256, float64, float128, ripemd160, sha256). Special type 'name' indicates an account name.
-	Index         string `json:"index_position,omitempty"` // Index number, 1 - primary (first), 2 - secondary index (in order defined by multi_index), 3 - third index, etc. Number or name of index can be specified, e.g. 'secondary' or '2'.
-	EncodeType    string `json:"encode_type,omitempty"`    // The encoding type of key_type (i64 , i128 , float64, float128) only support decimal encoding e.g. 'dec'" "i256 - supports both 'dec' and 'hex', ripemd160 and sha256 is 'hex' only
-	JSON          bool   `json:"json"`                     // JSON output if true, binary if false
-	IndexPosition uint32 `json:"index_position"`
-	Reverse       bool   `json:"reverse"`
-	ShowPayer     bool   `json:"show_payer"`
+	Code       string `json:"code"` // Contract "code" account where table lives
+	Scope      string `json:"scope"`
+	Table      string `json:"table"`
+	LowerBound string `json:"lower_bound,omitempty"`
+	UpperBound string `json:"upper_bound,omitempty"`
+	Limit      uint32 `json:"limit,omitempty"`          // defaults to 10 => chain_plugin.hpp:struct get_table_rows_params
+	KeyType    string `json:"key_type,omitempty"`       // The key type of --index, primary only supports (i64), all others support (i64, i128, i256, float64, float128, ripemd160, sha256). Special type 'name' indicates an account name.
+	Index      string `json:"index_position,omitempty"` // Index number, 1 - primary (first), 2 - secondary index (in order defined by multi_index), 3 - third index, etc. Number or name of index can be specified, e.g. 'secondary' or '2'.
+	EncodeType string `json:"encode_type,omitempty"`    // The encoding type of key_type (i64 , i128 , float64, float128) only support decimal encoding e.g. 'dec'" "i256 - supports both 'dec' and 'hex', ripemd160 and sha256 is 'hex' only
+	JSON       bool   `json:"json"`                     // JSON output if true, binary if false
+	Reverse    bool   `json:"reverse"`
+	ShowPayer  bool   `json:"show_payer"`
 }
 
 type GetTableRowsResp struct {
